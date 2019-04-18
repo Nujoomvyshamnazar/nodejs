270c7d39f2dcd48a2da83a4af46f9680f3d04c37
Your Name <nujoomvyshamnnazar@gmail.com>
2019-04-19 00:07:23 +0400

    using express module to run server@

---
+++ express/index.js
@@ -0,0 +1,14 @@
+// calling a Express module
+const express = require('express')
+
+// creating a server using express
+const app = express()
+
+
+
+
+app.listen(3000,()=>{
+    
+    
+    console.log('App listening on port 3000')
+})
