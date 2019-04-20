1e7553ddece9dd128ffdaafe716da9a5338b254f
Your Name <nujoomvyshamnnazar@gmail.com>
2019-04-20 16:13:26 +0400

    installing express and initiated server

---
+++ Blog/index.js
@@ -0,0 +1,16 @@
+// Calling Express  module After installing Express Package
+const express = require('express')
+
+
+// Creating Server Instance
+const app = new express()
+
+
+// listening to the port 4000
+app.listen(4000,()=>
+        {  
+
+ console.log('App Listening to the port 4000') 
+}
+        
+          )
