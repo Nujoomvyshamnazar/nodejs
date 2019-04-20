0499dff0d04088e6e7a6304663ec0169fa980531
Your Name <nujoomvyshamnnazar@gmail.com>
2019-04-20 17:37:03 +0400

    uploaded theme and pages

--- Blog/index.js
+++ Blog/index.js
@@ -1,9 +1,30 @@
 // Calling Express  module After installing Express Package
 const express = require('express')
 
+// calling path module
+const path = require('path')
+
 
 // Creating Server Instance
 const app = new express()
+
+// calling pages index.html
+
+app.get('/',(req,res)  =>
+       
+     {
+
+res.sendFile(path.resolve(__dirname,'pages/index.html'))
+             
+}  
+       
+       )
+
+
+// adding assets to your project
+
+app.use(express.static('public'))
+
 
 
 // listening to the port 4000
